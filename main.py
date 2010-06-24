fab4a0059e352e7b4944864202b0df2bb790db84
Evan Klitzke <evan@eklitzke.org>
2010-06-23 20:30:31 -0700

    fixes

--- main.py
+++ main.py
@@ -27,37 +27,12 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('main.html', ws_url="ws://iomonad.com:8214")
 
-class DataHandler(tornado.websocket.WebSocketHandler):
-
-    log = ClassLogger()
-
-    def open(self):
-        self.log.info('opened ws connection')
-        self.io_loop = tornado.ioloop.IOLoop.instance()
-        self.last_write = 0
-        self.receive_message(self.on_message)
-        self.write_message('foo')
-        self.log.info('done with open handler')
-
-    def on_message(self, msg):
-        self.log.info('on_message = %r' % (msg,))
-        self.write_message('hi')
-
-    def more_data(self):
-        now = time.time()
-        val = 1 + math.sin(now)
-        self.log.info('sending %s, %s' % (now, val))
-        self.write_message(simplejson.dumps({'millis': now * 1000, 'val': val}))
-        self.io_loop.add_callback(now + 1.0, self.more_data)
-
 settings = {
     'debug': True,
     'static_path': os.path.join(os.getcwd(), 'static')
 }
 
-application = tornado.web.Application([
-        ('/', MainHandler),
-        ('/websocket', DataHandler)], **settings)
+application = tornado.web.Application([('/graphomatic', MainHandler)], **settings)
 
 if __name__ == '__main__':
     configure_logging()
